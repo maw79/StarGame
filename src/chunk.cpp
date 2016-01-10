5ca01639d12a2af550a2778975367b8d47595b6b
aaron <arsgate@gmail.com>
2016-01-10 12:45:59 -0600

    implementing chunks (starting)

---
+++ src/chunk.cpp
@@ -0,0 +1,13 @@
+//
+// Created by aaron on 1/10/16.
+//
+
+#include "chunk.h"
+
+chunk::chunk(int z, int w)
+{
+    chunks.Stars();
+    chunks.Gen_Stars();
+    x = z;
+    y = w;
+}
---
+++ src/chunk.h
@@ -0,0 +1,20 @@
+//
+// Created by aaron on 1/10/16.
+//
+
+#ifndef OPENGL_CHUNK_H
+#define OPENGL_CHUNK_H
+
+#include "Stars.h"
+
+
+class chunk {
+public:
+    int x;
+    int y;
+    Stars chunks;
+    chunk(int z, int w);
+};
+
+
+#endif //OPENGL_CHUNK_H
