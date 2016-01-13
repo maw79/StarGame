adc968d812af4dac3d6887fd4db2e0bd61663e83
aaron <arsgate@gmail.com>
2016-01-13 13:08:03 -0600

    implementing chunks (starting)

--- src/main.cpp
+++ src/main.cpp
@@ -30,8 +30,7 @@ using std::endl;
 player ship;
 Stars star;
 
-int main (int argc, char** argv)
-{
+int main(int argc, char** argv) {
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_DOUBLE|GLUT_RGB|GLUT_DEPTH);
 
