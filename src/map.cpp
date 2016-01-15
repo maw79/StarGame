6394f21c4053022d9bce75a3c9f1c5ca39dc0ab8
aaron <arsgate@gmail.com>
2016-01-15 05:24:18 -0600

    starting map changes

---
+++ src/map.cpp
@@ -0,0 +1,10 @@
+//
+// Created by aaron on 1/15/16.
+//
+
+#include "map.h"
+
+map::map()
+{
+    chunk chunkq[200][200];
+}
---
+++ src/map.h
@@ -0,0 +1,17 @@
+//
+// Created by aaron on 1/15/16.
+//
+
+#ifndef OPENGL_MAP_H
+#define OPENGL_MAP_H
+
+#include "chunk.h"
+
+
+class map {
+public:
+    map();
+};
+
+
+#endif //OPENGL_MAP_H
